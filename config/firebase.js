69f97c7a90a3aded3cdbba6229e35ec0ee2df613
realtouseef <touseef69r@gmail.com>
2022-04-12 22:59:03 +0500

    removed the auth one, moving to firestore

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,5 +10,3 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,14 @@ import { useState } from "react";
 import Head from "next/head";
 import Paper from "/public/paper-plane.svg";
 
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../config/firebase";
-
 export default function Home() {
   const [email, setEmail] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    await signup(email.email);
-
     console.log(email);
   }
-
-  const signup = (email) => {
-    return createUserWithEmailAndPassword(auth, email);
-  };
 
   return (
     <>
