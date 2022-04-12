e56a26937abdf2d77e2ba9459f89ea339ade9bb8
realtouseef <touseef69r@gmail.com>
2022-04-13 01:38:25 +0500

    added basic security

--- config/firebase.js
+++ config/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import Head from "next/head";
 import Paper from "/public/paper-plane.svg";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 
 export default function Home() {
-  const [email, setEmail] = useState("");
+  const [sendEmail, setSendEmail] = useState("");
+  const [successMessage, setSuccessMessage] = useState(false);
+  const [subscribing, setSubscribing] = useState(false);
+  const emailRef = collection(db, "emails");
 
   async function handleSubmit(e) {
     e.preventDefault();
 
-    console.log(email);
+    setSubscribing(true);
+    await addDoc(emailRef, { email: sendEmail });
+    setSuccessMessage(true);
+    setSubscribing(false);
+
+    setTimeout(() => {
+      setSuccessMessage(false);
+      setSendEmail("");
+    }, 7000);
   }
 
   return (
@@ -21,20 +34,31 @@ export default function Home() {
         />
       </Head>
       <form
+        id="form"
         onSubmit={handleSubmit}
         className="grid place-content-center h-screen"
       >
         <center className="flex items-center justify-center">
           <input
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setSendEmail(e.currentTarget.value)}
             type="email"
+            value={sendEmail}
             placeholder="joe@joe.com"
             className="w-44 px-4 py-2 rounded-l-md focus:outline-none"
+            required
           />
-          <button className="h-10 bg-violet-500 hover:bg-violet-700  pr-2 py-1 rounded-r-md">
+          <button
+            disabled={subscribing}
+            className={`${
+              subscribing && "bg-violet-900 cursor-not-allowed"
+            } h-10 bg-violet-500 hover:bg-violet-700  pr-2 py-1 rounded-r-md`}
+          >
             <Paper className="ml-2 w-6 h-6" />
           </button>
         </center>
+        <small className="mt-1 text-white">
+          {successMessage && "Thank you for subscribing"}
+        </small>
       </form>
     </>
   );
