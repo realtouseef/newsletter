a1cb2cd1a9a0726f36778c9382f852f914cdf696
realtouseef <touseef69r@gmail.com>
2022-04-14 13:41:19 +0500

    fixed small bug, added const ACTION

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,16 @@ const initialState = {
   subscribing: false,
 };
 
+const ACTION = {
+  FIELD: "field",
+  SUCCESS_MESSAGE: "successMessage",
+  SUBSCRIBING: "subscribing",
+  SET_DEFAULT: "setDefault",
+};
+
 function handleEmails(state, action) {
   switch (action.type) {
-    case "field": {
+    case ACTION.FIELD: {
       return {
         ...state,
         [action.field]: action.value,
@@ -20,21 +27,21 @@ function handleEmails(state, action) {
       };
     }
 
-    case "successMessage": {
+    case ACTION.SUCCESS_MESSAGE: {
       return {
         ...state,
         successMessage: true,
       };
     }
-    case "subscribing": {
+    case ACTION.SUBSCRIBING: {
       return {
         ...state,
         subscribing: true,
       };
     }
-    case "setDefault": {
+    case ACTION.SET_DEFAULT: {
       return {
-        ...state,
+        [action.field]: "",
         sendEmail: "",
         subscribing: false,
         successMessage: false,
@@ -55,16 +62,14 @@ export default function Home() {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    dispatch({ type: "subscribing" });
+    await dispatch({ type: ACTION.SUBSCRIBING });
 
     await addDoc(emailRef, { email: sendEmail });
 
-    dispatch({ type: "successMessage" });
+    await dispatch({ type: ACTION.SUCCESS_MESSAGE });
 
-    // dispatch({ type: ACTION.SEND_EMAILS });
-
-    setTimeout(() => {
-      dispatch({ type: "setDefault" });
+    await setTimeout(() => {
+      dispatch({ type: ACTION.SET_DEFAULT });
     }, 3000);
   }
 
@@ -86,21 +91,25 @@ export default function Home() {
           <input
             onChange={(e) =>
               dispatch({
-                type: "field",
+                type: ACTION.FIELD,
                 value: e.target.value,
                 field: "email",
               })
             }
             type="email"
+            value={sendEmail}
             placeholder="joe@joe.com"
             className="w-44 px-4 py-2 rounded-l-md focus:outline-none"
             required
           />
           <button
             disabled={subscribing}
-            className={`${
-              subscribing && "bg-violet-900 cursor-not-allowed"
-            } h-10 bg-violet-500 hover:bg-violet-700  pr-2 py-1 rounded-r-md`}
+            type="submit"
+            className={
+              subscribing
+                ? "h-10 bg-violet-900 cursor-not-allowed pr-2 py-1 rounded-r-md"
+                : "h-10 bg-violet-500 hover:bg-violet-700 pr-2 py-1 rounded-r-md"
+            }
           >
             <Paper className="ml-2 w-6 h-6" />
           </button>
